6ff0d6216f44aeaef0550b7874db3da3509240ff
Johannes Fung <fyhannes@berkeley.edu>
2023-04-10 10:02:26 -0700

    added state tracking for mouse callback

--- src/callbacks.cpp
+++ src/callbacks.cpp
@@ -4,6 +4,14 @@
 #include <sstream>
 #include <stdexcept>
 
+/*
+ * We have these variables to keep track of the state of our mouse
+ * since GLFW does not explicitly support holding down a mouse button.
+ */
+bool left_click_down = false;
+double prev_xpos = 0;
+double prev_ypos = 0;
+
 void glfw_error_callback(int error, const char* description)
 {
     std::stringstream error_message;
@@ -21,11 +29,20 @@ void glfw_mouse_callback(__unused GLFWwindow* window,
                          __unused int action,
                          __unused int mods)
 {
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+    
     if (button == GLFW_MOUSE_BUTTON_LEFT)
     {
-        double xpos, ypos;
-        glfwGetCursorPos(window, &xpos, &ypos);
-        
-        std::cout << "xpos: " << xpos << " ypos: " << ypos << std::endl;
+        if (action == GLFW_RELEASE)
+            left_click_down = false;
+        else if (action == GLFW_PRESS)
+            left_click_down = true;
+    }
+    
+    if (!left_click_down)
+    {
+        prev_xpos = xpos;
+        prev_ypos = ypos;
     }
 }
